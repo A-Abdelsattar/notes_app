78bb5c41562ee703eec1c32f79d51ee1b2de5ec3
ahmed <ahmedabdelsattar28@gmail.com>
2023-04-16 21:24:11 +0200

    create addNote method

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/constante.dart';
 import 'package:notes_app/src/app_root.dart';
 
 void main() async{
   await Hive.initFlutter();
-  await Hive.openBox('notes');
+  await Hive.openBox(kNotesBox);
   runApp(const AppRoot());
 }
 
